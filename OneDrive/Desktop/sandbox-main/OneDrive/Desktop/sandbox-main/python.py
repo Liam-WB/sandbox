74e5e90741b0421190ce5df95a0bdd2f637651f1
A W <liamwartnerblake@gmail.com>
2025-03-14 08:48:21 +0000

    -Add python topics

--- OneDrive/Desktop/sandbox-main/OneDrive/Desktop/sandbox-main/python.py
+++ OneDrive/Desktop/sandbox-main/OneDrive/Desktop/sandbox-main/python.py
@@ -1,4 +1,16 @@
 # FUNDAMENTAL SKILLS
+# Variables
+# Conditions
+# Chained Conditionals
+# Operators
+# Control Flow
+# Loops/Iterables
+# Basic Data Structure
+# Functions
+# Mutable vs Immutable
+# Common Methods
+# File IO
+
 # INTERMIDIATE SKILLS
 # ADVANCED SKILLS
 # EXTRA SKILLS
