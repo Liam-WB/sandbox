cb7b7dd83e02e5c8a7e900b6f705d9946689c2d8
A W <liamwartnerblake@gmail.com>
2025-03-14 09:32:07 +0000

    -Add python.py functions

--- OneDrive/Desktop/sandbox-main/OneDrive/Desktop/sandbox-main/python.py
+++ OneDrive/Desktop/sandbox-main/OneDrive/Desktop/sandbox-main/python.py
@@ -7,6 +7,36 @@
 # Loops/Iterables
 # Basic Data Structure
 # Functions
+
+def example():
+    print("Item 1")
+    print("Item 2")
+
+    
+example()
+
+    # Parameters/Arguments
+
+def example2(request_1, request_2): # <-- Parameters for function (Inputs defined for the function)
+    print(f"This is {request_1} and {request_2}")
+
+
+example2("Item 1", "Item 2")
+example2(0, 1) # <-- Arguments (Actual values for the parameter)
+
+    # Function Types
+
+# Perform Task
+def value_example(val):
+    print(f"Here is {val}")
+
+# Return Item/Value
+def get_value_example(val):
+    return val
+
+message = get_value_example(23)
+print(message)
+
 # Mutable vs Immutable
 # Common Methods
 # File IO
