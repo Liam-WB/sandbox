c850db698ee9da84e9e5b5b9810062aa775401d2
A W <liamwartnerblake@gmail.com>
2025-03-24 08:24:05 +0000

    -Alter page placeholder content

--- OneDrive/Desktop/exodia-main/src/App.jsx
+++ OneDrive/Desktop/exodia-main/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import logo from './assets/LOGO.png'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] flex items-center justify-center flex-col">
+      <img 
+        src={ logo }
+        alt="Centered Image" 
+        className="mb-8 object-cover"
+        style={{ width: '26em', height: '26em' }} 
+      />
+      <h1 className="text-[6em] font-bold text-white">
+        Big things coming...
       </h1>
     </div>
   )
